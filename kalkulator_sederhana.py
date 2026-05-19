e9974aa05eb1f8af9cac34dc0ac974b93f678485
Muhammad Rosidin <didinrosidin401@gmail.com>
2026-05-19 18:44:06 +0700

    Create kalkulator_sederhana.py

---
+++ kalkulator_sederhana.py
@@ -0,0 +1,18 @@
+angka1 = float(input("masukan angka pertama anda: "))
+angka2 = float(input("masukan angka kedua anda: " ))
+operasi = input(" masukan operasi +,*,-,/: ")
+
+if operasi == "+":
+        print ( " hasil " , angka1 + angka2)
+elif operasi == "-":
+        print ( " hasil " , angka1 - angka2)
+elif operasi == "*":
+        print ( " hasil " , angka1 * angka2)
+elif operasi == "/":
+        # Di sini diganti pakai angka2, Din!
+        if angka2 != 0 :
+                print (" hasil " , angka1 / angka2)
+        else:
+                print ( " Eror: Tidak bisa dibagi dengan nol! " )
+else:
+        print (" isi bagian oprasi, angka1 dan angka2 terlebih dahulu")
