2d99c84d971d919f4466d498edf52e24c17c1010
Muhammad Rosidin <didinrosidin401@gmail.com>
2026-06-01 01:13:56 +0700

    Create seleksi_nilai_mahasiswa.py

---
+++ seleksi_nilai_mahasiswa.py
@@ -0,0 +1,13 @@
+print (" nilai mahasiswa ")
+
+mahasiswa = input(" masukan nama mahasiswa : ")
+nilai = float(input(" masukan nilai mahasiswa : "))
+
+if nilai >= 90 :
+        print (" nilai A nama kamu ", mahasiswa)
+elif nilai >= 70 :
+        print (" nilai B nama kamu ", mahasiswa)
+elif nilai >= 60 :
+        print (" nilai C nama kamu" , mahasiswa)
+else:
+        print ( " nilai jelk remedial  nama kamu ", mahasiswa )
