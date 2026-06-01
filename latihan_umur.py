26f01fe10d9aab7ecae58c8f5ae608f68e38ab27
Muhammad Rosidin <didinrosidin401@gmail.com>
2026-06-01 18:43:37 +0700

    Create latihan_umur.py

---
+++ latihan_umur.py
@@ -0,0 +1,12 @@
+print("umur manusia")
+
+umur = int(input("umur kamu? "))
+
+if umur >= 60:
+    print("umur kamu sudah tua")
+elif umur >= 19:
+    print("umur kamu masih muda/dewasa")
+elif umur >= 12:
+    print("umur kamu masih remaja") # Diganti remaja biar makin gaco logikanya!
+else:
+    print("masih bayi/balita")
